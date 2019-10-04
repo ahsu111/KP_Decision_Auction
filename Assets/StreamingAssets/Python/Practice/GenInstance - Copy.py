0cce69fb15bcc1e3943264aa683bc9cd514bcc79
Tonyhsu111 <anthonyhsu.au@gmail.com>
2019-10-04 12:58:41 +1000

    no message

---
+++ Assets/StreamingAssets/Python/Practice/GenInstance - Copy.py
@@ -0,0 +1,11 @@
+for i in range(1,11):
+    print(i)
+    with open(f"i{i}.txt") as f:
+        lines = f.readlines()
+        lines = [l for l in lines]
+        for j in range(i,11,16): #16+i
+            print("j is equal to", j)
+            with open(f"k{j}.txt", "w") as f1:
+                f1.writelines(lines)
+            f1.close()
+    f.close()
